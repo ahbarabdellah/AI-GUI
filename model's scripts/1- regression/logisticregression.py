f968d7954b96c21e27a865b44a94d8498cf2ca58
hamido <hamidabdellaoui55@gmail.com>
2022-01-04 19:32:32 +0100

    logistic_reg script (need verification)

---
+++ model's scripts/1- regression/logisticregression.py
@@ -0,0 +1,50 @@
+import warnings 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as se 
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.preprocessing import LabelEncoder 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report,plot_confusion_matrix
+warnings.filterwarnings('ignore')
+
+def NullClearner(df):
+    if(isinstance(df, pd.Series) and (df.dtype in ["float64","int64"])):
+        df.fillna(df.mean(),inplace=True)
+        return df
+    elif(isinstance(df, pd.Series)):
+        df.fillna(df.mode()[0],inplace=True)
+        return df
+    else:return df
+def EncodeX(df):
+    return pd.get_dummies(df)
+def EncodeY(df):
+    if len(df.unique())<=2:
+        return df
+    else:
+        un_EncodedT=np.sort(pd.unique(df), axis=-1, kind='mergesort')
+        df=LabelEncoder().fit_transform(df)
+        EncodedT=[xi for xi in range(len(un_EncodedT))]
+        print("Encoded Target: {} to {}".format(un_EncodedT,EncodedT))
+        return df
+    
+    
+def logisticregression(path,features,target):
+    file_path=path
+    df=pd.read_csv(file_path)
+    X = df[features]
+    Y = df[target]
+    x=X.columns.to_list()
+    for i in x:
+        X[i]=NullClearner(X[i])  
+    X=EncodeX(X)
+    Y=EncodeY(NullClearner(Y))
+    x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.2,random_state=123)
+    x_train,y_train = RandomOverSampler(random_state=123).fit_resample(x_train, y_train)
+        # Build Model here
+    model = LogisticRegression(random_state = 123,n_jobs = -1)
+    model.fit(x_train, y_train)
+    return model
+    
