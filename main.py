77d1dc88cc5f2c277bfc357b6222a051b452af0b
hamido <hamidabdellaoui55@gmail.com>
2022-01-26 14:34:30 +0100

    new architecture

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 #### ------ pour importet les fichier python en dehor de ce dossier  ------- ####
 import sys 
 import os
-sys.path.append(os.path.abspath("./2. Notebook to script/1- regression"))
-sys.path.append(os.path.abspath("./2. Notebook to script/2- classification"))
+sys.path.append(os.path.abspath("SCRIPTS"))
 import regressionmodels
 import classificationmodels
 
  
 #################################
 #### regression
-path="./3. Test Data/Regression/House prices/House prices.csv"
+path="./TestData/Regression/House prices/House prices.csv"
 features=['MSSubClass','MSZoning','LotFrontage','LotArea','Street','Neighborhood','HouseStyle','YearBuilt','RoofStyle','Foundation','BedroomAbvGr','Functional','Fireplaces','GarageType','GarageYrBlt','GarageFinish','GarageCars','GarageArea','GarageQual','GarageCond','PoolArea','PoolQC']
 target='SalePrice'
 accuracy, r2, score2, score3, model = regressionmodels.RFRegressor(path,features,target)
-print('Randomgorest :\t\t\t',r2)
+print('Random Forest Regressor:\t\t',r2)
 
 
 #################################
 #### classification
-path = "./3. Test Data/Classification/winequality-red.csv"
+path = "./TestData/Classification/winequality-red.csv"
 features = ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']
 target   = 'quality'
 model,x_train,x_test,y_train,y_test = classificationmodels.rfc(path,features,target)
-print('\n\ Random forest Classifier : ',model.score(x_test,y_test)*100)
+print('\nRandom forest Classifier: \t\t',model.score(x_test,y_test)*100,'\n')
